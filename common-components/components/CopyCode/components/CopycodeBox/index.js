d49577a9a365f4e93aab21ddf66813413d99c923
harrisong.gutierrez <harrisong.gutierrez@smbssolutions.com>
2024-02-09 13:42:21 -0600

    add common-com[pnent

---
+++ common-components/components/CopyCode/components/CopycodeBox/index.js
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import PropTypes from "prop-types";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+import { FaRegCopy } from "react-icons/fa";
+import { FaCopy } from "react-icons/fa";
+import Button from "@/components/Button";
+
+const CopyCodeBox = ({ text, onClick }) => {
+  const [isTextCopied, setIsTextCopied] = useState(false);
+  const handleCopyText = () => {
+    setIsTextCopied(true);
+    onClick();
+  };
+  return (
+    <CopyToClipboard text={text}>
+      <Button
+        onClick={handleCopyText}
+        text={text}
+        href="/"
+        isRedirected={false}
+        styleLink="Offer-copyBox"
+      >
+        {text}
+        {isTextCopied ? <FaCopy /> : <FaRegCopy />}
+      </Button>
+    </CopyToClipboard>
+  );
+};
+
+CopyCodeBox.propTypes = {
+  text: PropTypes.string,
+};
+
+export default CopyCodeBox;
