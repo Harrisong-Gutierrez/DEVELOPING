b4f35cbba4224c7a9bb4f9616e9e2805c9861830
Harrisong <harrygutie7@gmail.com>
2024-02-11 20:21:06 -0600

    respnsive already done

--- Sergent-Major/components/Categories/index.js
+++ Sergent-Major/components/Categories/index.js
@@ -1,30 +1,26 @@
+import React from 'react';
 import PropTypes from "prop-types";
 import Category from "./components/Category";
 import { categories } from "@/constants/dataList";
-import { v4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 export const Categories = ({ title }) => {
-  const renderCategory = () => {
-    return categories.map((category) => {
-      return (
-        <div>
-          <Category
-            key={v4()}
-            text={category.text}
-            alt={category.alt}
-            width={category.width}
-            height={category.height}
-            src={category.src}
-          />
-        </div>
-      );
-    });
-  };
-
   return (
     <section className="Categories">
       <h2 className="Categories-title">{title}</h2>
-      <div className="Categories-content">{renderCategory()}</div>
+      <div className="Categories-content">
+        {categories.map(category => (
+          <div key={uuidv4()}>
+            <Category
+              text={category.text}
+              alt={category.alt}
+              width={category.width}
+              height={category.height}
+              src={category.src}
+            />
+          </div>
+        ))}
+      </div>
     </section>
   );
 };
